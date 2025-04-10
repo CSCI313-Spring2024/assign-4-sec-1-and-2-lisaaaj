f2276b862775dc904f8d69a9accd802698f38e1a
lisaaaj <abcdelaj@gmail.com>
2025-04-10 09:42:32 -0500

    assignment change

---
+++ Downloads/assign-4-sec-1-and-2-lisaaaj-main/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { EditContactComponent } from './edit-contact/edit-contact.component';
+import { ContactsComponent } from './contacts/contacts.component';
+
+
+export const routes: Routes = [
+  { path: '', component: ContactsComponent, title: 'Contact List' },
+  { path: 'add', component: AddContactComponent, title: 'Add Contact' },
+  { path: 'edit/:id', component: EditContactComponent, title: 'Edit Contact' },
+];
---
+++ Downloads/assign-4-sec-1-and-2-lisaaaj-main/src/app/contacts/contacts.component.ts
@@ -0,0 +1,37 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Contact } from '../contact.data';
+import { Router, RouterLink } from '@angular/router';
+import { ContactService } from '../contact.service';
+
+@Component({
+  selector: 'app-contacts',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './contacts.component.html',
+  styleUrl: './contacts.component.css',
+})
+export class ContactsComponent implements OnInit {
+  contactService = inject(ContactService);
+
+  contacts: Contact[] = [];
+
+  ngOnInit() {
+    this.contacts = this.contactService.getContacts();
+    console.log('Loaded contacts:', this.contacts);
+  }
+
+  deleteContact(id: number): void {
+    this.contactService.deleteContact(id);
+    this.contacts = this.contactService.getContacts();
+  }
+
+  trackById(index: number, contact: Contact): number {
+    return contact.id;
+  }
+
+  constructor(private router: Router) {}
+  goToEdit(id: number): void {
+    this.router.navigate(['/edit', id]);
+  }
+}
